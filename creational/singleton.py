d010ef08d5524594d15d353eb97828bf8b41beaa
SaiAshish <saiashish3760@gmail.com>
2024-07-02 13:48:47 +0530

    k

--- creational/singleton.py
+++ creational/singleton.py
@@ -14,3 +14,16 @@ class Singleton:
         return cls._instance
 
 singleton_instance = Singleton.get_instance()
+
+class Singleton:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Singleton, cls).__new__(cls)
+        return cls._instance
+
+singleton1 = Singleton()
+singleton2 = Singleton()
+
+print(singleton1 is singleton2)  # True
