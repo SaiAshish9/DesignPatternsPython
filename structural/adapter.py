e5395b544866f41cb4fada237db309fd263bd85e
SaiAshish <saiashish3760@gmail.com>
2024-07-02 16:36:21 +0530

    k

---
+++ structural/adapter.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+class TemperatureProvider(ABC):
+    @abstractmethod
+    def get_temperature(self):
+        pass
+
+class TemperatureSensor:
+    def get_temperature_celsius(self):
+        return 25  
+        
+class TemperatureAdapter(TemperatureProvider):
+    def __init__(self, sensor):
+        self.sensor = sensor
+
+    def get_temperature(self):
+        celsius_temperature = self.sensor.get_temperature_celsius()
+        fahrenheit_temperature = celsius_temperature * 9 / 5 + 32
+        return fahrenheit_temperature
+
+if __name__ == "__main__":
+    sensor = TemperatureSensor()
+    adapter = TemperatureAdapter(sensor)
+    temperature_fahrenheit = adapter.get_temperature()
+    print(f"Temperature in Fahrenheit: {temperature_fahrenheit:.2f}°F")
