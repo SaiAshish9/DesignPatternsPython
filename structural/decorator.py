8d1fcd76fe158e0dd659e94df352857ab8a0747b
SaiAshish <saiashish3760@gmail.com>
2024-07-02 16:27:11 +0530

    k

--- structural/decorator.py
+++ structural/decorator.py
@@ -5,7 +5,6 @@ class Coffee(ABC):
     def cost(self):
         pass
 
-
 class SimpleCoffee(Coffee):
     def cost(self):
         return 5  
@@ -13,26 +12,21 @@ class SimpleCoffee(Coffee):
 class Milk(Coffee):
     def __init__(self, coffee):
         self._coffee = coffee
-
     def cost(self):
         return self._coffee.cost() + 2  
 
 class Sugar(Coffee):
     def __init__(self, coffee):
         self._coffee = coffee
-
     def cost(self):
         return self._coffee.cost() + 1  
 
 if __name__ == "__main__":
     simple_coffee = SimpleCoffee()
     print(f"Cost of Simple Coffee: ${simple_coffee.cost()}")
-
     coffee_with_milk = Milk(simple_coffee)
     print(f"Cost of Coffee with Milk: ${coffee_with_milk.cost()}")
-
     coffee_with_sugar = Sugar(simple_coffee)
     print(f"Cost of Coffee with Sugar: ${coffee_with_sugar.cost()}")
-
     coffee_with_milk_and_sugar = Milk(Sugar(simple_coffee))
     print(f"Cost of Coffee with Milk and Sugar: ${coffee_with_milk_and_sugar.cost()}")
