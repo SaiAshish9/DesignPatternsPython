6f0cc92daa59bc72feac09fa58e968fb9ddd1b57
SaiAshish <saiashish3760@gmail.com>
2024-07-02 20:03:58 +0530

    k

--- behavioral/memento.py
+++ behavioral/memento.py
@@ -1,33 +1,26 @@
 class Memento:
     def __init__(self, state):
         self._state = state
-
     def get_state(self):
         return self._state
 
 class Originator:
     def __init__(self):
         self._state = None
-
     def set_state(self, state):
         self._state = state
-
     def get_state(self):
         return self._state
-
     def save_state_to_memento(self):
         return Memento(self._state)
-
     def get_state_from_memento(self, memento):
         self._state = memento.get_state()
 
 class CareTaker:
     def __init__(self):
         self._memento_list = []
-
     def add(self, state):
         self._memento_list.append(state)
-
     def get(self, index):
         return self._memento_list[index]
 
