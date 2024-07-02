3176fb6b1f4499a5259d524cbb99cf19e407c543
SaiAshish <saiashish3760@gmail.com>
2024-07-02 20:22:06 +0530

    k

--- behavioral/template.py
+++ behavioral/template.py
@@ -18,20 +18,16 @@ class Game(ABC):
 class Cricket(Game):
     def end_play(self):
         print("Cricket Game Finished!")
-
     def initialize(self):
         print("Cricket Game Initialized! Start playing.")
-
     def start_play(self):
         print("Cricket Game Started. Enjoy the game!")
 
 class Football(Game):
     def end_play(self):
         print("Football Game Finished!")
-
     def initialize(self):
         print("Football Game Initialized! Start playing.")
-
     def start_play(self):
         print("Football Game Started. Enjoy the game!")
 
