6785230bb0f7c3a317ea4724c1ed9c70fcc6263d
SaiAshish <saiashish3760@gmail.com>
2024-07-02 13:49:48 +0530

    k

--- creational/singleton.py
+++ creational/singleton.py
@@ -27,3 +27,19 @@ singleton1 = Singleton()
 singleton2 = Singleton()
 
 print(singleton1 is singleton2)  # True
+
+
+def singleton(cls):
+    instances = {}
+
+    def get_instance(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+
+    return get_instance
+
+@singleton
+class Singleton:
+    def __init__(self):
+        pass
