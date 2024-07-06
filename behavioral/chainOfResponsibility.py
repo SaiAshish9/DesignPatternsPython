eccc9b4c53f1a5aa6334f64cc11b9649b864df43
SaiAshish <saiashish3760@gmail.com>
2024-07-06 13:39:46 +0530

    k

--- behavioral/chainOfResponsibility.py
+++ behavioral/chainOfResponsibility.py
@@ -33,16 +33,15 @@ class President(PurchaseHandler):
     def handle_request(self, amount):
         print(f"President handles the request for ${amount}")
 
-if __name__ == "__main__":
-    manager = Manager()
-    director = Director()
-    vice_president = VicePresident()
-    president = President()
-    manager.set_successor(director)
-    director.set_successor(vice_president)
-    vice_president.set_successor(president)
-    amounts = [500, 1500, 7000, 12000]
+manager = Manager()
+director = Director()
+vice_president = VicePresident()
+president = President()
+manager.set_successor(director)
+director.set_successor(vice_president)
+vice_president.set_successor(president)
+amounts = [500, 1500, 7000, 12000]
 
-    for amount in amounts:
-        print(f"\nProcessing purchase request for ${amount}:")
-        manager.handle_request(amount)
+for amount in amounts:
+    print(f"\nProcessing purchase request for ${amount}:")
+    manager.handle_request(amount)
